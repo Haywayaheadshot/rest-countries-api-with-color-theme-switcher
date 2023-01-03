0d92c37a733bb93171892baa997c3bc08df7e9ce
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-01-03 00:32:14 +0000

    write on change to filter onchange for by region and country name. Also use NavLink to direct clicked country to details

--- src/components/dep/DisplayCountries.js
+++ src/components/dep/DisplayCountries.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCountries } from '../../redux/home/Home';
 import magnifier from '../../assets/icons/magnifier.png';
@@ -6,6 +7,7 @@ import '../../styles/display-countries.css';
 
 function DisplayCountries() {
   const [search, setSearch] = useState('');
+  const [filterRegion, setFilterRegion] = useState('');
   const countries = useSelector((state) => state.countries);
 
   const filteredCountries = [countries];
@@ -18,8 +20,14 @@ function DisplayCountries() {
 
   //   Search the input for countries
   if (search !== '') {
-    mappedCountries = filteredCountries.filter((country) => country
+    mappedCountries = countries.filter((country) => country.name.official
       .toLowerCase().includes(search.toLowerCase()));
+  }
+
+  // Filter by region
+  if (filterRegion !== '') {
+    mappedCountries = countries.filter((country) => country.region
+      .toLowerCase().includes(filterRegion.toLowerCase()));
   }
 
   return (
@@ -28,33 +36,28 @@ function DisplayCountries() {
         <img className="magnifier-img" src={magnifier} alt="magnifier" />
         <input type="text" className="search-input" placeholder="Search for a country ..." onChange={(e) => setSearch(e.target.value)} />
       </section>
-      <select className="select-pop">
-        <option value="Find by Region">Filter by Region</option>
-        <option value="Africa">Africa</option>
-        <option value="America">America</option>
-        <option value="Asia">Asia</option>
-        <option value="Europe">Europe</option>
-        <option value="Oceania">Oceania</option>
-      </select>
+      <input placeholder="Find by Region.." onChange={(e) => setFilterRegion(e.target.value)} className="select-pop" />
       {mappedCountries.map((country) => (
-        <section className="countries-section" key={country.name.official}>
-          <img className="country-flag-png" src={country.flags.png} alt={country.name.official} />
-          <div className="section-mobile-info">
-            <h1>{country.name.official}</h1>
-            <h4>
-              Population:
-              <span className="section-mobile-info-data">{country.population}</span>
-            </h4>
-            <h4>
-              Region:
-              <span className="section-mobile-info-data">{country.region}</span>
-            </h4>
-            <h4>
-              Capital:
-              <span className="section-mobile-info-data">{country.capital}</span>
-            </h4>
-          </div>
-        </section>
+        <NavLink to={`/${country.name.official}`} key={country.name.official}>
+          <section className="countries-section" key={country.name.official}>
+            <img className="country-flag-png" src={country.flags.png} alt={`Flag of ${country.name.official}`} />
+            <div className="section-mobile-info">
+              <h1>{country.name.official}</h1>
+              <h4>
+                Population:
+                <span className="section-mobile-info-data">{country.population}</span>
+              </h4>
+              <h4>
+                Region:
+                <span className="section-mobile-info-data">{country.region}</span>
+              </h4>
+              <h4>
+                Capital:
+                <span className="section-mobile-info-data">{country.capital}</span>
+              </h4>
+            </div>
+          </section>
+        </NavLink>
       ))}
     </div>
   );
